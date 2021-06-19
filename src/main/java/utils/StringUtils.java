17f7bab03912322f17b378a71d061b4672994c62
crio-xx <little_yo@list.ru>
2021-06-19 21:34:23 +0300

    Выполнено задание: Сценарий должен состоять из следующих частей:
        1) регистрация новой учётной записи с достаточно уникальным адресом электронной почты (чтобы не конфликтовало с ранее созданными пользователями, в том числе при предыдущих запусках того же самого сценария),
        2) выход (logout), потому что после успешной регистрации автоматически происходит вход,
        3) повторный вход в только что созданную учётную запись,
        4) и ещё раз выход.

---
+++ src/main/java/utils/StringUtils.java
@@ -0,0 +1,28 @@
+package utils;
+
+import java.util.Random;
+
+public class StringUtils {
+
+    public static String getRandomString(int length) {
+        return getRandomSequence(length, 97, 122);
+    }
+
+    public static String getRandomName(int length) {
+        return getRandomSequence(1, 65, 90) + getRandomString(length - 1);
+    }
+
+    public static String getRandomNumber(int length) {
+        return getRandomSequence(length, 48, 57);
+    }
+
+
+    public static String getRandomSequence(int length, int leftLimit, int rightLimit) {
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+}
---
+++ src/test/java/client/CheckRegistration.java
@@ -0,0 +1,51 @@
+package client;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import ui.BaseTest;
+import utils.StringUtils;
+
+public class CheckRegistration extends BaseTest {
+
+    @Test
+    public void testRegistration() {
+        String firstName = StringUtils.getRandomName(5);
+        String lastName = StringUtils.getRandomName(7);
+        String address = StringUtils.getRandomName(10) + ", " + StringUtils.getRandomName(6) + ", №" + StringUtils.getRandomNumber(3);
+        String postcode = StringUtils.getRandomNumber(5);
+        String city = StringUtils.getRandomName(6);
+        String email = StringUtils.getRandomString(10) + "@" + StringUtils.getRandomString(5) + ".com";
+        String password = StringUtils.getRandomString(4) + StringUtils.getRandomNumber(4) + StringUtils.getRandomString(4);
+        String phone = "+"+StringUtils.getRandomNumber(11);
+        String country = "United States";
+        String zone = "California";
+
+        driver.navigate().to(baseUrl + "/create_account");
+
+        // user data entry for registration
+        driver.findElement(By.name("firstname")).sendKeys(firstName);
+        driver.findElement(By.name("lastname")).sendKeys(lastName);
+        driver.findElement(By.name("address1")).sendKeys(address);
+        driver.findElement(By.name("postcode")).sendKeys(postcode);
+        driver.findElement(By.name("city")).sendKeys(city);
+        driver.findElement(By.name("phone")).sendKeys(phone);
+        new Select(driver.findElement(By.name("country_code"))).selectByVisibleText(country);
+        new Select(driver.findElement(By.name("zone_code"))).selectByVisibleText(zone);
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.name("confirmed_password")).sendKeys(password);
+
+
+        // click Register
+        driver.findElement(By.name("create_account")).click();
+
+        // logout
+        driver.findElement(By.cssSelector("#box-account [href $= logout]")).click();
+
+        // login
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.name("login")).click();
+    }
+}
