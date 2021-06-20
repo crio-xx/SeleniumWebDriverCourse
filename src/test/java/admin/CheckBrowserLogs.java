82e13c4c259120f6fc1c54b96b7c96c842342fe7
crio-xx <little_yo@list.ru>
2021-06-20 22:24:47 +0300

    Реализован сценарий, который проверяет, не появляются ли в логе браузера сообщения при открытии страниц в учебном приложении, а именно -- страниц товаров в каталоге в административной панели:     1) зайти в админку     2) открыть каталог, категорию, которая содержит товары (страница http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1)     3) последовательно открывать страницы товаров и проверять, не появляются ли в логе браузера сообщения (любого уровня)

---
+++ src/test/java/admin/CheckBrowserLogs.java
@@ -0,0 +1,72 @@
+package admin;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import ui.BaseTest;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CheckBrowserLogs extends BaseTest {
+
+    @Test
+    public void TestCheckBrowserLogs() {
+        HashSet<String> viewedCategory = new HashSet<>();
+        HashSet<String> viewedProduct = new HashSet<>();
+        Queue<String> categoryIds = new LinkedList<>();
+        String catalogUrl = baseUrl + "/admin/?app=catalog&doc=catalog";
+        Login.auth(driver, catalogUrl, "admin", "admin");
+        categoryIds.add("category_id=0");
+        do {
+
+            String currentCategory = categoryIds.remove();
+            driver.navigate().to(catalogUrl + "&" + currentCategory);
+
+            var links = driver.findElements(By.cssSelector("#content table tr td:nth-child(2) [href]"));
+            for (var link : links) {
+                String categoryId = getCategoryId(link.getAttribute("href"));
+                if (categoryId != null && !viewedCategory.contains(categoryId) && !categoryIds.contains(categoryId)) {
+                    categoryIds.add(categoryId);
+                }
+            }
+
+
+            List<String> productsInCurrentCategory = new LinkedList<>();
+            var products = driver.findElements(By.cssSelector(
+                    String.format("#content table td:nth-child(2) [href *= '%s&product_id='] ", currentCategory)));
+
+            for(var product : products) {
+                productsInCurrentCategory.add(product.getAttribute("href"));
+            }
+
+            for (var productLink : productsInCurrentCategory) {
+                if(!viewedProduct.contains(productLink)) {
+                    checkBrowserLog(productLink);
+                    viewedProduct.add(productLink);
+                }
+            }
+            viewedCategory.add(currentCategory);
+
+        } while (!categoryIds.isEmpty());
+
+    }
+
+    private String getCategoryId(String href) {
+        Pattern pattern = Pattern.compile("category_id=\\d$");
+        Matcher matcher = pattern.matcher(href);
+        if (matcher.find()) {
+            return href.substring(matcher.start(), matcher.end());
+        }
+        return null;
+    }
+
+    private void checkBrowserLog(String href) {
+        driver.navigate().to(href);
+        assert driver.manage().logs().get("browser").getAll().size() == 0;
+        driver.navigate().back();
+    }
+}
---
+++ src/test/java/client/CheckBrowserLogs.java
@@ -0,0 +1,35 @@
+package client;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import ui.BaseTest;
+
+public class CheckBrowserLogs extends BaseTest {
+
+    @Test
+    public void TestCheckStickers() {
+        driver.navigate().to(baseUrl);
+        var logsBefore = driver.manage().logs().get("browser").getAll();
+
+        Select manufactures = new Select(driver.findElement(By.name("manufacturer_id")));
+        var countManufactures = manufactures.getOptions().size();
+
+        for (int i = 1; i < countManufactures; i++) {
+            new Select(driver.findElement(By.name("manufacturer_id"))).selectByIndex(i);
+            var countProducts = driver.findElements(By.cssSelector("[class ^= 'product']")).size();
+            for (int j = 1; j <= countProducts; j++) {
+                driver.findElement(
+                        By.cssSelector(String.format("[class ^= 'product']:nth-child(%d)", j))).click();
+                driver.navigate().back();
+            }
+            driver.navigate().to(baseUrl);
+        }
+
+        var logsAfter = driver.manage().logs().get("browser").getAll();
+        if(logsBefore.size() > 0 && logsAfter.size() > 0) {
+            logsAfter.removeAll(logsBefore);
+        }
+        assert logsAfter.size() == 0;
+    }
+}
