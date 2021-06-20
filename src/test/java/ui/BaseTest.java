4b4e312983ab64078dea4344561528fe62d1a625
crio-xx <little_yo@list.ru>
2021-06-20 16:53:07 +0300

    Реализован сценарий для добавления товаров в корзину и удаления товаров из корзины:     1) открыть главную страницу     2) открыть первый товар из списка     3) добавить его в корзину (при этом может случайно добавиться товар, который там уже есть, ничего страшного)     4) подождать, пока счётчик товаров в корзине обновится     5) вернуться на главную страницу, повторить предыдущие шаги ещё два раза, чтобы в общей сложности в корзине было 3 единицы товара     6) открыть корзину (в правом верхнем углу кликнуть по ссылке Checkout)     7) удалить все товары из корзины один за другим, после каждого удаления подождать, пока внизу обновится таблица

--- src/test/java/ui/BaseTest.java
+++ src/test/java/ui/BaseTest.java
@@ -5,15 +5,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseTest {
     public WebDriver driver;
     public String baseUrl = "http://localhost/litecart";
+    public WebDriverWait wait;
 
     @Before
     public void setUpChrome(){
         System.setProperty("webdriver.chrome.driver", ConfigurationUtils.getChromeDriverPath());
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 5);
     }
 
     @After
